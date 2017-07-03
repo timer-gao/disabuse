38bb4aa6eeaa55f74438bf0f7e6d8033aaef3ea7
gaomengjun <mengjungao@163.com>
2017-07-03 10:53:46 +0800

    Initial commit

---
+++ src/main/java/edu/ssdut/DisabuseApplication.java
@@ -0,0 +1,12 @@
+package edu.ssdut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisabuseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisabuseApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/ssdut/DisabuseApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.ssdut;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DisabuseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
