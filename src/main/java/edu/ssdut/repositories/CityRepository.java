3957c4b6c2c4a48ac0516887fe198d4701b36417
gaomengjun <mengjungao@163.com>
2017-07-17 09:08:18 +0800

    完成提问页面、首页主要功能，实体类有改动

--- src/main/java/edu/ssdut/repositories/CityRepository.java
+++ src/main/java/edu/ssdut/repositories/CityRepository.java
@@ -9,5 +9,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface CityRepository extends JpaRepository<City,Long> {
-
+    City findCityById(Long id);
+    City findCityByName(String name);
 }
